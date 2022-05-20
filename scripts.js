cb5c81cb713f024a5df377891f306dc4bcbfc103
Your NameAliAlMowed <you@example.comalmowed.ali@gmail.com>
2022-05-20 16:48:48 +0300

    Adding the boxes and audios varaiables

---
+++ scripts.js
@@ -0,0 +1,12 @@
+var greenBox = document.getElementById("green-box");
+var redBox = document.getElementById("red-box");
+var blueBox = document.getElementById("blue-box");
+var yellowBox = document.getElementById("yellow-box");
+var mainText = document.getElementById("main-text");
+
+var greenAudio = new Audio("assets/audio/green.mp3");
+var redAudio = new Audio("assets/audio/red.mp3");
+var blueAudio = new Audio("assets/audio/blue.mp3");
+var yellowAudio = new Audio("assets/audio/yellow.mp3");
+var wrongAudio = new Audio("assets/audio/wrong.mp3");
+
