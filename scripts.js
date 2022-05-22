ca5cf8517d1fb6ee08659771c856b06898631f2d
Your NameAliAlMowed <you@example.comalmowed.ali@gmail.com>
2022-05-22 12:05:00 +0300

    Adding boxes Clicked and wrong click functions

--- scripts.js
+++ scripts.js
@@ -18,3 +18,56 @@ var sequence = [];
 var maxLevel = 16;
 var level = 0;
 var pattern = [];
+
+//Every time game choose a box:
+function greenBoxClicked() {
+    if (start = true) {
+        greenBox.classList.add("active");
+        greenAudio.play();
+        setTimeout(function () {
+            greenBox.classList.remove("active");
+        }, 200);
+    }
+}
+function redBoxClicked() {
+    if (start = true) {
+        redBox.classList.add("active");
+        redAudio.play();
+        setTimeout(function () {
+            redBox.classList.remove("active");
+        }, 200);
+    }
+}
+function blueBoxClicked() {
+    if (start = true) {
+        blueBox.classList.add("active");
+        blueAudio.play();
+        setTimeout(function () {
+            blueBox.classList.remove("active");
+        }, 200);
+    }
+}
+function yellowBoxClicked() {
+    if (start = true) {
+        yellowBox.classList.add("active");
+        yellowAudio.play();
+        setTimeout(function () {
+            yellowBox.classList.remove("active");
+        }, 200);
+    }
+}
+function wrongClick() {
+    game.classList.add("active-game");
+    greenBox.classList.add("active");
+    redBox.classList.add("active");
+    blueBox.classList.add("active");
+    yellowBox.classList.add("active");
+    wrongAudio.play();
+    setTimeout(function () {
+        game.classList.remove("active-game");
+        greenBox.classList.remove("active");
+        redBox.classList.remove("active");
+        blueBox.classList.remove("active");
+        yellowBox.classList.remove("active");
+    }, 500)
+}
